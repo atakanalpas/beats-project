9265f60566c5c4aca2d5bf8e8b0d6ed95210cc8f
atakanalpas <atakanaslantas27@gmail.com>
2025-12-17 14:04:14 +0000

    Route fixing

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,12 @@
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/prisma"
+import { prisma } from "@/lib/prisma";
 
-
-export default NextAuth({
+const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -12,5 +14,10 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "database",
+  },
   secret: process.env.NEXTAUTH_SECRET,
-})
+});
+
+export { handler as GET, handler as POST };
